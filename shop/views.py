95305e2943fce9eefae4cfc41583d167956e5c7a
Xender01231 <pugaland@gmail.com>
2024-02-17 20:24:30 +0400

    123

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,30 @@
-from .models import Product
-from django.shortcuts import render
+from shop.models import Product, Order
+from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def catalog(request):
     all_products = Product.objects.all() 
     return render(request, 'shop/catalog.html', {'all_products': all_products})
 
-def create_order(request):
-    return render(request, 'shop/create_order.html')
+def create_order(request, id):
+    print(request.GET)
+    if request.method == 'POST':
+        Order.objects.create(
+            product_id = id,
+            delivery_address = request.POST['delivery_address']
+        )
+        return redirect('orders')
+
+    product = Product.objects.get(id=id)
+    return render(request, 'shop/create_order.html', {'product': product})
 
 def orders(request):
-    return render(request, 'shop/order.html')
+    orders = Order.objects.all() 
+    return render(request, 'shop/orders.html', {'all_orders': orders })
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+
+
